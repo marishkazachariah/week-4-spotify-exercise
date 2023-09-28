75ef6ad4b141bfa00126c4f3215f0dc38ae255f3
Laura Beattie <laurabeattie@lauras-air.speedport.ip>
2023-09-28 14:49:53 +0200

    Added loan class

---
+++ LoanClass.java
@@ -0,0 +1,14 @@
+Public Class Loan{
+
+public static void main(String[]args){
+
+String book;
+String member;
+Double dueDate;
+
+public static initiateLoan(String book, String member, double dueDate);
+
+public static terminateLoan(String book, String member, double dueDate);
+
+
+
