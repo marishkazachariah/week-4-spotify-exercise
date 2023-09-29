27ef685475ab6103dbaaf03a77c58c1b9e4f36fc
marishkazachariah <marishka.zachariah@gmail.com>
2023-09-29 14:52:03 +0200

    add course to Main class

--- src/main/java/exercise_4/Main.java
+++ src/main/java/exercise_4/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         College yale = new College();
         System.out.println("New student added");
+        System.out.println("New course added");
     }
 }
